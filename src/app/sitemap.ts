3b3793b3d522e79bc3ddad773ce36b71a0c911f4
Zack2051 <uniqlo.cs13@nycu.edu.tw>
2026-02-21 00:38:35 +0800

    Update base URL in sitemap function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://bnb-site.vercel.app";
+  const baseUrl = "https://bnb-site-fifw.vercel.app";
 
   const paths = [
     "/",
